2b736ab397624052841760e9cbb9f5f32c563951
JensBrandels <brandelsjens@gmail.com>
2023-11-07 11:34:09 +0100

    added maxInput function

--- script.js
+++ script.js
@@ -2,7 +2,6 @@
 //1. Skapa array med ord
 
 let words = ['happy', 'chair', 'water', 'smile', 'philosopher', 'cat', 'diamond']
-console.log(words);
 //2. Skapa initiate game knapp
 
 let gameStart = document.querySelector('#header__button')
@@ -12,22 +11,44 @@ gameStart.addEventListener('click', ()=>{
 
 });
 
-
 //3. slumpa ut random ord med math.random och lägg i en tom array
-function getRandomWord(words){
+function getRandomWord(randomWord){
 
-    const randomIndex = words[Math.floor(Math.random() * words.length)]; 
+    const randomIndex = randomWord[Math.floor(Math.random() * randomWord.length)]; 
     
-
+    //skriv ut det nya ordet på hemsidan
     let displayWord = document.querySelector('.main__randomWord')
     displayWord.innerHTML = randomIndex;
+
     
-    return randomIndex;
+    //lägger ordet i array splittad med 1 bokstav per index
+    let splitWord = randomIndex.split('')
+
+    return splitWord;
+
+    // console.log(splitWord);
 }
-console.log(getRandomWord(words));
-//4. Gör den tomma arrayens nyinlagda string till : ['s't'r'i'n'g'] dynamisk nyckel ? 
 //5. input fält som bara kan ha en bokstav som alternativ
-//6. Jämför den mot den dynamiska nyckeln på submit knappen (enter)
+
+let maxInput = document.getElementById('inputText').addEventListener('input', function(inputMax){
+    let maxChars = 1;
+
+    if(inputMax.target.value.length > maxChars) {
+        inputMax.target.value = inputMax.target.value.substr(0, maxChars);
+    }
+})
+
+
+
+//6. Jämför den mot den splittade stringen på submit knappen (enter)
+function compareCharacters(split, input) {
+    let inputValue = input.document.getElementById('inputText').value
+    split = getRandomWord(words)
+
+    if(inputValue === )
+}
+
+
 //7. If else sats inkluderar bokstaven gör en funktion som skriver ut den
 //8. annars kasta in bostaven i en ny tom array och gör det synligt på hemsidan
 //9. + display image och kör en countdown på de 6 försöken
