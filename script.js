4f8e7e483155ca6aa2f5109434c984e7a508c548
Marcus Widen <hellowiden@gmail.com>
2023-11-08 10:28:32 +0100

    commit

--- script.js
+++ script.js
@@ -107,5 +107,94 @@ function gameOver() {
 
 /*
 
+let words = ['happy', 'chair', 'water', 'smile', 'philosopher', 'cat', 'diamond'];
+
+let currentWord;
+let guessedLetters;
+let remainingTries;
+
+document.addEventListener('DOMContentLoaded', () => {
+
+currentWord = getRandomWord(words);
+guessedLetters = [];
+remainingTries = 6;
+
+const startButton = document.getElementById('header__button');
+const inputText = document.getElementById('inputText');
+const submitButton = document.getElementById('submitButton');
+const randomWordDisplay = document.querySelector('.main__randomWord');
+const countdownCircle = document.querySelector('.main__countdown--circle');
+const wrongUsedWords = document.querySelector('.main__wrongUsedWords');
+
+startButton.addEventListener('click', () => {
+  // Reset game variables and UI elements
+  currentWord = getRandomWord(words);
+  guessedLetters = [];
+  remainingTries = 6;
+  randomWordDisplay.textContent = currentWord.replace(/[a-zA-Z]/g, '_ ');
+  countdownCircle.textContent = remainingTries;
+  wrongUsedWords.textContent = '';
+  inputText.value = '';
+});
+
+submitButton.addEventListener('click', (e) => {
+e.preventDefault(); // Prevent form submission (page refresh)
+const inputLetter = inputText.value.trim().toLowerCase();
+
+    if (inputLetter.length !== 1 || !inputLetter.match(/[a-z]/)) {
+  alert('Please enter a valid letter (a-z).');
+  return;
+}
+
+  if (guessedLetters.includes(inputLetter)) {
+  alert('You already guessed that letter.');
+  return;
+}
+
+    guessedLetters.push(inputLetter);
+if (!currentWord.includes(inputLetter)) {
+  remainingTries--;
+}
+
+updateWordDisplay();
+updateCountdown();
+updateWrongUsedWords();
+inputText.value = '';
+
+if (remainingTries === 0) {
+  alert(`Game Over! The word was "${currentWord}".`);
+  inputText.disabled = true; // Disable further input if the game is over.
+} else if (currentWord.split('').every(letter => guessedLetters.includes(letter))) {
+  alert('Congratulations! You Win!');
+  inputText.disabled = true; // Disable further input if the game is won.
+}
+});
+
+function getRandomWord(wordList) {
+const randomIndex = Math.floor(Math.random() * wordList.length);
+return wordList[randomIndex];
+}
+
+function updateWordDisplay() {
+  let display = '';
+  for (const letter of currentWord) {
+    if (guessedLetters.includes(letter)) {
+      display += letter;
+    } else {
+      display += '_';
+    }
+    display += ' ';
+  }
+  randomWordDisplay.textContent = display;
+}
+
+function updateCountdown() {
+  countdownCircle.textContent = remainingTries;
+}
+
+  function updateWrongUsedWords() {
+  wrongUsedWords.textContent = guessedLetters.join(' ');
+}
+});
 
 */ 
